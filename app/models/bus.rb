06ab65ea1438e0748778a782e942d5749fbcee69
Xolani Mfuko & Linda Gibson <student@epicodus.com>
2014-09-08 11:57:41 -0700

    establish association where bus has many lines

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -2,4 +2,5 @@ class Bus < ActiveRecord::Base
 
   validates :name, :presence => true
   has_many :stops
+  has_many :lines, through: :stops
 end
--- spec/models/bus_spec.rb
+++ spec/models/bus_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe Bus do
   it { should validate_presence_of :name }
   it { should have_many :stops }
+  it { should have_many :lines }
 end
