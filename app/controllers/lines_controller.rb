65b0c0073d37c2f964dca1ee89e42f85a6786fff
Xolani Mfuko & Linda Gibson <student@epicodus.com>
2014-09-08 14:48:56 -0700

    create mvc for show page

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -3,4 +3,8 @@ class LinesController < ApplicationController
   def index
     @lines = Line.all
   end
+
+  def show
+    @lines = Line.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :buses
-end
 
   resources :lines
 end
