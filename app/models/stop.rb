2390336d554a84797fbe4b4ac850c33f3467537e
Xolani Mfuko & Linda Gibson <student@epicodus.com>
2014-09-08 11:47:46 -0700

    validate a unique relationship between bus_id and line_id

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -2,7 +2,9 @@ class Stop < ActiveRecord::Base
 
   validates :bus_id, presence: true
   validates :line_id, presence: true
+  validates_uniqueness_of :bus_id, scope: :line_id
 
   belongs_to :bus
   belongs_to :line
+
 end
--- spec/models/stop_spec.rb
+++ spec/models/stop_spec.rb
@@ -6,4 +6,5 @@ describe Stop do
   it { should validate_presence_of :line_id }
   it { should belong_to :bus }
   it { should belong_to :line }
+  it { should validate_uniqueness_of(:bus_id).scoped_to(:line_id) }
 end
