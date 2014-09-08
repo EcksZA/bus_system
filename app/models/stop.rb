3731873648076acb786c4e150064b79d39270008
Xolani Mfuko & Linda Gibson <student@epicodus.com>
2014-09-08 11:38:54 -0700

    validate presence of line_id for stops

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,4 +1,5 @@
 class Stop < ActiveRecord::Base
 
   validates :bus_id, presence: true
+  validates :line_id, presence: true
 end
--- spec/models/stop_spec.rb
+++ spec/models/stop_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe Stop do
 
   it { should validate_presence_of :bus_id }
+  it { should validate_presence_of :line_id }
 end
