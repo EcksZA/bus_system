6efccf4fe2568080ce7d54d7b2d92d8e2dda6627
Xolani Mfuko & Linda Gibson <student@epicodus.com>
2014-09-08 16:49:10 -0700

    create mvc for create functionality for stops

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -4,4 +4,21 @@ class StopsController < ApplicationController
     @lines = Line.all
     @stop = Stop.new
   end
+
+  def create
+    @buses = Bus.all
+    @lines = Line.all
+    @stop = Stop.new(stop_params)
+    if @stop.save
+      flash[:notice] = "New Stop was created!"
+      redirect_to new_stop_path
+    else
+      render 'new'
+    end
+  end
+
+private
+  def stop_params
+    params.require(:stop).permit(:bus_id, :line_id)
+  end
 end
